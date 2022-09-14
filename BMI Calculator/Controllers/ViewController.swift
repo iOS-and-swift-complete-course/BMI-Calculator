48773081c7967c4ca23c186568ba75d97f4aaced
Chris Hand <cj.hand@gmail.com>
2022-09-14 08:42:50 -0700

    Wire up the sliders

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let format = String(format: "%.1f", sender.value)
+        heightLabel.text = format + "m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let format = String(format: "%.1f", sender.value)
+        weightLabel.text = format + "Kg"
+    }
+    
+    @IBAction func calculateClicked(_ sender: UIButton) {
+    }
 }
 
